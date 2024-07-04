ebb1d03e1b0fe444e65e0474878a1e56e3e8c183
reem <reemdiaa@gmail.com>
2024-07-04 13:24:26 +0100

    Created a command help message

---
+++ constants.py
@@ -0,0 +1,7 @@
+COMMAND_HELP_MSG = """
+The commands you can use with the Mitch Bot are:
+1. .roll [dice]+[mod] - i will roll the dice and display the outcome
+2. .bag - i will display all the items in the bag of holding
+3. .hold [item] - i will add the item to the bag of holding
+4. .remove [item] - i will remove the item from the bag of holding
+"""
