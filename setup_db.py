4421722140b7847940c3019b786bb0aa6a2c9b64
reem <reemdiaa@gmail.com>
2024-07-04 13:25:05 +0100

    Integrated the database setup to running the bot

--- setup_db.py
+++ setup_db.py
@@ -4,7 +4,7 @@ from player import Player
 from character import Character, CharacterClass, Race, race_enum_mapping, class_enum_mapping
 
 
-def create_tables():
+def create_tables(cursor, mydb):
     # Create the players table
     cursor.execute("""
             CREATE TABLE IF NOT EXISTS players (
@@ -25,52 +25,40 @@ def create_tables():
             FOREIGN KEY (player_id) REFERENCES players(player_id)
         )
     """)
-    print("Table characters was created successfully")
 
-
-def setup_characters(character):
-    mysql_race = race_enum_mapping[character.race]
-    mysql_char_class = class_enum_mapping[character.char_class]
-
+    # Create the "bag_of_holding" table if it doesn't exist
     cursor.execute("""
-        INSERT INTO characters (name, race, class, player_id)
-        VALUES (%s, %s, %s, %s)
-    """, (character.name, mysql_race, mysql_char_class, character.player_id))
+                CREATE TABLE IF NOT EXISTS bag_of_holding (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    item VARCHAR(255) NOT NULL,
+                    quantity INT NOT NULL DEFAULT 1,
+                    description VARCHAR(255)
+                )
+            """)
 
     mydb.commit()
+    print("All tables were created successfully")
 
-    print('Inserted Character')
+# def setup_characters(character, cursor, mydb):
+#     mysql_race = race_enum_mapping[character.race]
+#     mysql_char_class = class_enum_mapping[character.char_class]
+#
+#     cursor.execute("""
+#         INSERT INTO characters (name, race, class, player_id)
+#         VALUES (%s, %s, %s, %s)
+#     """, (character.name, mysql_race, mysql_char_class, character.player_id))
+#
+#     mydb.commit()
+#
+#     print('Inserted Character')
+#
 
-
-def setup_player(player):
-    cursor.execute("""
-        INSERT INTO players (player_name, discord_username)
-        VALUES (%s, %s)
-    """, (player.player_name, player.discord_username))
-
-    mydb.commit()
-
-    print('Inserted Player')
-
-
-if __name__ == '__main__':
-    try:
-        mydb = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            database='dungeons_and_dragons'
-        )
-
-        if mydb.is_connected():
-            cursor = mydb.cursor()
-
-            create_tables()
-
-            # reem = Player('Reem', 'lasoninja')
-            # setup_player(reem)
-            #
-            # celestia = Character('Celestia', Race.AASIMAR, CharacterClass.WIZARD, 1)
-            # setup_characters(celestia)
-
-    except mysql.connector.Error as err:
-        print('Error connecting to mysql-db-setup', err)
+# def setup_player(player, cursor, mydb):
+#     cursor.execute("""
+#         INSERT INTO players (player_name, discord_username)
+#         VALUES (%s, %s)
+#     """, (player.player_name, player.discord_username))
+#
+#     mydb.commit()
+#
+#     print('Inserted Player')
