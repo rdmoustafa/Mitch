e967da92771e65df6cf388b96798dae064e1ac7c
reem <reemdiaa@gmail.com>
2024-07-04 14:29:35 +0100

    Updated the command help message

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 COMMAND_HELP_MSG = """
-The commands you can use with the Mitch Bot are:
-1. .roll [dice]+[mod] - i will roll the dice and display the outcome
-2. .bag - i will display all the items in the bag of holding
-3. .hold [item] - i will add the item to the bag of holding
-4. .remove [item] - i will remove the item from the bag of holding
+The Mitch bot uses `.` as the prefix to any commands, you can do the following:
+1. roll [dice]+[mod] - i will roll the dice and display the outcome
+2. bag - i will display all the items in the bag of holding
+3. hold [item] | add [item] - i will add the item to the bag of holding
+4. remove [item] - i will remove the item from the bag of holding
 """
